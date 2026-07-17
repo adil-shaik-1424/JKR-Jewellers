baf7d0c752d13c8437e65a599e7822b05c01c0fb
Shaik Adil <adilshaik1424@gmail.com>
2026-07-17 17:10:39 +0530

    Fix adminApi.js to use environment variable

--- jkr-jewellers-frontend/src/admin/services/adminApi.js
+++ jkr-jewellers-frontend/src/admin/services/adminApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const adminApi = axios.create({
 
-    baseURL: "http://localhost:8081/api/admin",
+    baseURL: `${import.meta.env.VITE_API_BASE_URL}/admin`,
 
     headers: {
         "Content-Type": "application/json",
